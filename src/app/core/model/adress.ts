e69ffb5afba68100f395de3a68a9a2cc76f2a36d
Unknown <marcel.pautz@gmail.com>
2017-10-04 13:27:08 +0200

    ADDED: Model Enteties Added
    Added the old/new entities

---
+++ src/app/core/model/adress.ts
@@ -0,0 +1,8 @@
+import {EntityObject} from "./entity-object";
+
+export class Adress extends EntityObject{
+  public city:string;
+  public postalCode:string;
+  public street:string;
+  public number:string;
+}
---
+++ src/app/core/model/person.ts
@@ -0,0 +1,9 @@
+import {EntityObject} from "./entity-object";
+
+export class Person extends EntityObject{
+  public firstName:string;
+  public lastName:string;
+  public email:string;
+  public picture:string;
+  public phoneNumber:string;
+}
