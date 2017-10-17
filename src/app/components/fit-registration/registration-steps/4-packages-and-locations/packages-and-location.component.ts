dbb63a1b9cc2b80e2dcdc220d1d26ad0ab616ec3
Keppi <simon.kepplinger@gmail.com>
2017-10-17 20:42:07 +0200

    Package selection

--- src/app/components/fit-registration/registration-steps/4-packages-and-locations/packages-and-location.component.ts
+++ src/app/components/fit-registration/registration-steps/4-packages-and-locations/packages-and-location.component.ts
@@ -7,7 +7,17 @@ import { Component } from '@angular/core';
 })
 export class PackagesAndLocationComponent {
 
+  public selectedPackage: number = 1;
+
   public constructor() {
   }
 
+  public togglePackage(packageNumber: number): void {
+    console.log(this.selectedPackage);
+    this.selectedPackage ^= packageNumber;
+  }
+
+  public isPackageSelected(packageNumber: number): boolean {
+    return (this.selectedPackage & packageNumber) !== 0;
+  }
 }
