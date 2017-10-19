dfe83e5faa17e02840f7622204e1b7c868517e59
Keppi <simon.kepplinger@gmail.com>
2017-10-19 09:19:36 +0200

    Fixed TagInput layout

--- src/app/components/fit-registration/registration-steps/2-detailed-data/tag-input/tag-input.component.ts
+++ src/app/components/fit-registration/registration-steps/2-detailed-data/tag-input/tag-input.component.ts
@@ -7,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TagInputComponent implements OnInit {
 
+  public tags: string[];
   public focus = false;
 
   public constructor() {
   }
 
   public ngOnInit() {
+  }
+
+  public removeItem(index: number): void {
+    this.tags.splice(index, 1);
   }
 }
