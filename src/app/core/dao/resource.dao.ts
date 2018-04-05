3533706e5a4332f53fc76dac782271ffdb5313a3
Simon Kepplinger <simon.kepplinger@gmail.com>
2018-04-06 01:56:57 +0200

    Tried to fix something, but failed

--- src/app/core/dao/resource.dao.ts
+++ src/app/core/dao/resource.dao.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AppConfig } from '../app-config/app-config.service';
 import { HttpClient } from '@angular/common/http';
-import { ResourceBooking } from '../model/resource';
+import { ResourceBooking } from '../model/resource-booking';
 
 @Injectable()
 export class ResourceDAO {
