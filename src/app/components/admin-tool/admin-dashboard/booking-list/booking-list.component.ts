a973d1472084751349020ab9f91df2ccc41774f2
Simon Kepplinger <simon.kepplinger@gmail.com>
2018-04-11 17:08:26 +0200

    Improved code

--- src/app/components/admin-tool/admin-dashboard/booking-list/booking-list.component.ts
+++ src/app/components/admin-tool/admin-dashboard/booking-list/booking-list.component.ts
@@ -10,50 +10,47 @@ import { CsvCreatorService } from '../../services/csv-creator.service';
 
 import { Subscription } from 'rxjs/Subscription';
 import { SortService } from '../../../../core/app-services/sort-service.service';
+import { SortHelper } from '../../../../core/app-helper/sort-helper';
+import { ColumnSortCriteria } from '../../../../core/app-helper/helper-model/column-sort-criteria';
+import { FitPackage } from '../../../../core/model/enums/fit-package';
+import { ArrayUtils } from '../../../../core/utils/array-utils';
 
 @Component({
   selector: 'fit-booking-list',
   templateUrl: './booking-list.component.html',
   styleUrls: ['./booking-list.component.scss']
 })
-export class BookingListComponent implements OnInit, OnDestroy {
+export class BookingListComponent implements OnInit {
 
   @Output()
   public sorted = new EventEmitter();
 
+  public displayedBookings: Booking[];
   public bookings: Booking[];
 
   public loading: boolean = true;
   public imageDownloadLink: string;
-  public tmpBookings: Booking[];
 
-  public checkedBasic: string = '-1';
-  public checkedSponsor: string = '-1';
-  public checkedPremium: string = '-1';
+  public companyFilter: string = '';
+  public locationFilter: string = '';
 
-  public nameSearch: string = '';
-  public placeSearch: string = '';
+  public displayedPackages: FitPackage[] = [FitPackage.BasicPack, FitPackage.SponsorPack, FitPackage.LecturePack];
 
-  private columnSortedSubscription: Subscription;
+  public helper: Booking[];
 
   public constructor(private bookingDAO: BookingDAO,
                      private eventService: EventService,
                      private appConfig: AppConfig,
                      private router: Router,
                      private bookingTransferService: BookingTransferService,
-                     private csvCreatorService: CsvCreatorService,
-                     private sortService: SortService) {
+                     private csvCreatorService: CsvCreatorService) {
     this.imageDownloadLink = this.appConfig.serverURL + '/media';
-    console.log(this.imageDownloadLink);
   }
 
   public async ngOnInit(): Promise<void> {
     this.bookings = await this.bookingDAO.fetchAllBookingsForEvent(this.eventService.selectedEvent.getValue());
-    this.tmpBookings = this.bookings;
+    this.displayedBookings = this.bookings;
     this.loading = false;
-    this.columnSortedSubscription = this.sortService.onColumnSorted.subscribe(event => {
-      this.sorted.emit(event);
-    });
   }
 
   public routeToCsvExport(): void {
@@ -66,111 +63,34 @@ export class BookingListComponent implements OnInit, OnDestroy {
     this.router.navigate(['/admin-tool', 'anmeldung', booking.id]);
   }
 
-  public ngOnDestroy(): void {
-    if (this.columnSortedSubscription != null) {
-      this.columnSortedSubscription.unsubscribe();
+  public onSorted(criteria: ColumnSortCriteria): void {
+    this.displayedBookings = this.bookings.sort((a, b) => SortHelper.sortHandler(a, b, criteria));
+  }
+
+  public togglePackage(fitPackage: FitPackage) {
+    if (this.displayedPackages.indexOf(fitPackage) === -1) {
+      this.displayedPackages.push(fitPackage);
+    } else {
+      ArrayUtils.deleteElement(this.displayedPackages, fitPackage);
     }
+    this.filterBookings();
   }
 
-  public onSorted(event: any): void {
-    this.sortList(event);
-  }
+  public filterBookings(): void {
+    this.displayedBookings = this.bookings
+      .filter((booking: Booking) => {
 
-  private sortList($event: CustomerSearchCriteria): void {
-    this.bookings = this.getCustomers($event);
-  }
+        let condition: boolean = this.displayedPackages.indexOf(booking.fitPackage.discriminator) !== -1;
 
-
-  getCustomers(criteria: CustomerSearchCriteria): Booking[] {
-    console.log(criteria.sortColumn);
-    console.log();
-    var alphas: string[];
-    alphas = criteria.sortColumn.split('.');
-    return this.bookings.sort((a, b) => {
-      if (alphas.length == 1) {
-        if (criteria.sortDirection === 'desc') {
-          if (a[criteria.sortColumn] < b[criteria.sortColumn])
-            return 1;
-          if (a[criteria.sortColumn] == b[criteria.sortColumn])
-            return 0;
-          if (a[criteria.sortColumn] > b[criteria.sortColumn])
-            return -1;
+        if (condition && booking.company != null) {
+          condition = booking.company.name.toLowerCase().includes(this.companyFilter.toLowerCase());
         }
-        else {
 
-          if (a[criteria.sortColumn] < b[criteria.sortColumn])
-            return -1;
-          if (a[criteria.sortColumn] == b[criteria.sortColumn])
-            return 0;
-          if (a[criteria.sortColumn] > b[criteria.sortColumn])
-            return 1;
+        if (condition && booking.location != null) {
+          condition = booking.location.number.toLowerCase().includes(this.locationFilter.toLowerCase())
         }
-      } else {
-        if (criteria.sortDirection === 'desc') {
-          if (a[alphas[0]][alphas[1]] < b[alphas[0]][alphas[1]])
-            return 1;
-          if (a[alphas[0]][alphas[1]] == b[alphas[0]][alphas[1]])
-            return 0;
-          if (a[alphas[0]][alphas[1]] > b[alphas[0]][alphas[1]])
-            return -1;
-        }
-        else {
 
-          if (a[alphas[0]][alphas[1]] < b[alphas[0]][alphas[1]])
-            return -1;
-          if (a[alphas[0]][alphas[1]] == b[alphas[0]][alphas[1]])
-            return 0;
-          if (a[alphas[0]][alphas[1]] > b[alphas[0]][alphas[1]])
-            return 1;
-        }
-      }
-    });
+        return condition;
+      });
   }
-
-  public searchName() {
-    this.bookings = this.tmpBookings.filter(c => c.company.name.toLowerCase().includes(this.nameSearch.toLowerCase()));//.filter(e=>e.location.number.toLowerCase().includes(this.placeSearch.toLowerCase()));
-  }
-
-  public helper: Booking[];
-
-  public tickCheckbox(tmp: number) {
-    this.helper = [];
-    if (tmp == 0) {
-      if (this.checkedBasic == 'Basispaket')
-        this.checkedBasic = '-1';
-      else
-        this.checkedBasic = 'Basispaket';
-    }
-    if (tmp == 1) {
-      if (this.checkedSponsor == 'Sponsorpaket')
-        this.checkedSponsor = '-1';
-      else
-        this.checkedSponsor = 'Sponsorpaket';
-    }
-    if (tmp == 2) {
-      if (this.checkedPremium == 'Vortragspaket')
-        this.checkedPremium = '-1';
-      else
-        this.checkedPremium = 'Vortragspaket';
-    }
-    if (this.checkedBasic == '-1' && this.checkedSponsor == '-1' && this.checkedPremium == '-1') {
-      this.bookings = this.tmpBookings;
-    }
-    else {
-      this.bookings = [];
-      console.log(this.helper);
-      this.bookings = this.bookings.concat(this.tmpBookings.filter(c => c.fitPackage.name.toLowerCase()
-        .includes(this.checkedBasic.toLowerCase())), this.tmpBookings.filter(c => c.fitPackage.name.toLowerCase()
-        .includes(this.checkedSponsor.toLowerCase())), this.tmpBookings.filter(c => c.fitPackage.name.toLowerCase()
-        .includes(this.checkedSponsor.toLowerCase())));
-      console.log(this.helper);
-    }
-
-  }
-
-}
-
-export class CustomerSearchCriteria {
-  sortColumn: string;
-  sortDirection: string;
 }
