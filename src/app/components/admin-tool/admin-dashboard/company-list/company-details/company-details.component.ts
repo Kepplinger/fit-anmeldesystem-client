57b5a0706daacb76fea408bb4ab9d8b4a8477088
Simon Kepplinger <simon.kepplinger@gmail.com>
2018-04-15 23:41:00 +0200

    Company tag edit

--- src/app/components/admin-tool/admin-dashboard/company-list/company-details/company-details.component.ts
+++ src/app/components/admin-tool/admin-dashboard/company-list/company-details/company-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { CompanyTransferService } from '../../../../../core/app-services/transfer-services/company-transfer.service';
 import { Company } from '../../../../../core/model/company';
+import { ArrayUtils } from '../../../../../core/utils/array-utils';
 
 @Component({
   selector: 'fit-company-details',
@@ -11,6 +12,7 @@ import { Company } from '../../../../../core/model/company';
 export class CompanyDetailsComponent implements OnInit {
 
   public company: Company;
+  public tagInput: string = '';
 
   public constructor(private activatedRoute: ActivatedRoute,
                      private router: Router,
@@ -28,6 +30,15 @@ export class CompanyDetailsComponent implements OnInit {
           }
         }
       });
+  }
+
+  public addTag(): void {
+    this.company.tags.push(this.tagInput);
+    this.tagInput = '';
+  }
+
+  public removeTag(tag: string): void {
+    ArrayUtils.deleteElement(this.company.tags, tag);
   }
 
 }
--- src/app/core/model/company.ts
+++ src/app/core/model/company.ts
@@ -8,6 +8,8 @@ export class Company {
   public address: Address;
   public contact: Contact;
 
+  public tags: string[];
+
   public name: string;
   public isPending: boolean;
 
@@ -15,6 +17,7 @@ export class Company {
                      contact?: Contact,
                      name?: string,
                      isPending?: boolean,
+                     tags?: string[],
                      id?: number,
                      timestamp?: string) {
     this.id = id;
@@ -22,6 +25,7 @@ export class Company {
     this.address = address;
     this.contact = contact;
     this.name = name;
+    this.tags = tags;
     this.isPending = isPending;
 
     if (this.address == null) {
@@ -30,6 +34,10 @@ export class Company {
 
     if (this.contact == null) {
       this.contact = new Contact();
+    }
+
+    if (this.tags == null) {
+      this.tags = [];
     }
   }
 }
