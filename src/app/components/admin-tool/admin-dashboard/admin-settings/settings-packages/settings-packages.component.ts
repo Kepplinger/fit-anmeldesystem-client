ae70c324cef5fa3a05d121a0624ae974139f9e99
Keppi <simon.kepplinger@gmail.com>
2018-08-28 15:42:43 +0200

    Small little changes

--- src/app/components/admin-tool/admin-dashboard/admin-settings/settings-packages/settings-packages.component.ts
+++ src/app/components/admin-tool/admin-dashboard/admin-settings/settings-packages/settings-packages.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { BaseSettingsChangesComponent } from '../../../../../core/base-components/base-settings-changes.component';
+
 import { PackageDAO } from '../../../../../core/dao/package.dao';
 import { FitPackage } from '../../../../../core/model/enums/fit-package';
 import { Package } from '../../../../../core/model/package';
 import { PackageHelper } from '../../../../../core/model/helper/package-helper';
+import { BaseSettingsChangesComponent } from '../../../../../core/base-components/base-settings-changes.component';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'fit-settings-packages',
@@ -15,7 +17,8 @@ export class SettingsPackagesComponent extends BaseSettingsChangesComponent impl
   public packages: Package[] = [];
   public editingPackage: Package;
 
-  public constructor(private packageDAO: PackageDAO) {
+  public constructor(private packageDAO: PackageDAO,
+                     private toastr: ToastrService) {
     super();
   }
 
@@ -40,11 +43,18 @@ export class SettingsPackagesComponent extends BaseSettingsChangesComponent impl
 
   public cancelEditing(fitPackage: Package): void {
     this.packages[this.packages.findIndex(p => p.id === fitPackage.id)] = PackageHelper.clone(this.editingPackage);
+    this.setUnsavedChanges(false);
     this.editingPackage = null;
   }
 
   public async updatePackage(fitPackage: Package): Promise<void> {
     this.packages = await this.packageDAO.updatePackage(fitPackage);
+    this.setUnsavedChanges(false);
+    this.toastr.success('Das Paket wurde erfolgreich geändert.', 'Paket gespeichert');
     this.editingPackage = null;
+  }
+
+  public packageChanged(): void {
+    this.setUnsavedChanges(true);
   }
 }
